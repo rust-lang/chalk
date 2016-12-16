d4aa6f5c13a235168cad87816af2a37390cf6eba
Niko Matsakis <niko@alum.mit.edu>
2016-12-16 05:22:13 -1000

    support multiple -p options

--- chalk/src/cli/mod.rs
+++ chalk/src/cli/mod.rs
@@ -18,9 +18,9 @@ Usage: chalk [options]
 Loads a chalk program and tries to solve the given goal.
 
 Options:
-    -p <file>, --program <file>     Specifies that we should load program clauses from `file`.
-    -g <goal>, --goal <goal>        Specifies the goal to try to solve. [default: goal(?X)]
-    --prolog                        Use the Prolog strategy, rather than Rust
+    -p <file>, --program <file> ...     Specifies that we should load program clauses from `file`.
+    -g <goal>, --goal <goal>            Specifies the goal to try to solve. [default: goal(?X)]
+    --prolog                            Use the Prolog strategy, rather than Rust
 ";
 
 #[derive(Debug, RustcDecodable)]
