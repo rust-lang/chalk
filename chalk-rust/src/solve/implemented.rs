3dc38aaee78edafb0c24f63c9d25ad631bd7cb8b
Niko Matsakis <niko@alum.mit.edu>
2017-01-17 04:38:52 -0500

    remove the preference for solutions found in environment
    The compiler *does* do something like this but it'd be nice to
    see if we can find an alternative.

--- chalk-rust/src/solve/implemented.rs
+++ chalk-rust/src/solve/implemented.rs
@@ -11,6 +11,11 @@ pub struct Implemented<'s> {
     env_goal: Quantified<InEnvironment<TraitRef>>,
 }
 
+enum Technique {
+    WithClause(usize),
+    WithImpl(ItemId),
+}
+
 impl<'s> Implemented<'s> {
     pub fn new(solver: &'s mut Solver, env_goal: Quantified<InEnvironment<TraitRef>>) -> Self {
         Implemented {
@@ -23,25 +28,25 @@ impl<'s> Implemented<'s> {
         let Implemented { solver, env_goal } = self;
         let program = solver.program.clone();
 
-        // First try to find a solution in the environment.
         let environment = &env_goal.value.environment;
         let num_clauses = environment.clauses.len();
-        let env_result = solver.solve_any(0..num_clauses, &env_goal, |solver, clause_index| {
-            MatchClause::new(solver, &env_goal, clause_index).solve()
+
+        let techniques = (0..num_clauses)
+            .map(Technique::WithClause)
+            .chain(program.impl_data.keys().map(|&impl_id| Technique::WithImpl(impl_id)));
+
+        let result = solver.solve_any(techniques, &env_goal, |solver, technique| {
+            match technique {
+                Technique::WithClause(clause_index) => {
+                    MatchClause::new(solver, &env_goal, clause_index).solve()
+                }
+                Technique::WithImpl(impl_id) => {
+                    ImplementedWithImpl::new(solver, env_goal.clone(), impl_id).solve()
+                }
+            }
         });
 
-        // If we found something in the environment, even if it might
-        // have caused us to infer things that are not necessarily
-        // correct, then take it. This is not obviously the right
-        // thing to do but it models rustc's current behavior.
-        if let Ok(v) = env_result {
-            return Ok(v);
-        }
-
-        // Nothing in the environment, so try impls.
-        solver.solve_any(&program.impl_data, &env_goal, |solver, (&impl_id, _impl_data)| {
-            ImplementedWithImpl::new(solver, env_goal.clone(), impl_id).solve()
-        }).chain_err(|| {
+        result.chain_err(|| {
             format!("`{:?}{:?}` is not implemented for `{:?}` in environment `{:?}`",
                     env_goal.value.goal.trait_id,
                     debug::Angle(&env_goal.value.goal.parameters[1..]),
--- chalk-rust/src/solve/normalize.rs
+++ chalk-rust/src/solve/normalize.rs
@@ -12,6 +12,11 @@ pub struct SolveNormalize<'s> {
     env_goal: Quantified<InEnvironment<Normalize>>,
 }
 
+enum Technique {
+    WithClause(usize),
+    WithImpl(ItemId),
+}
+
 impl<'s> SolveNormalize<'s> {
     pub fn new(solver: &'s mut Solver, env_goal: Quantified<InEnvironment<Normalize>>) -> Self {
         SolveNormalize {
@@ -27,25 +32,21 @@ impl<'s> SolveNormalize<'s> {
         // First try to find a solution in the environment.
         let environment = env_goal.value.environment.clone();
         let num_clauses = environment.clauses.len();
-        let env_result = solver.solve_any(0..num_clauses, &env_goal, |solver, clause_index| {
-            MatchClause::new(solver, &env_goal, clause_index).solve()
+        let techniques = (0..num_clauses)
+            .map(Technique::WithClause)
+            .chain(program.impl_data.keys().map(|&impl_id| Technique::WithImpl(impl_id)));
+
+        let result = solver.solve_any(techniques, &env_goal, |solver, technique| {
+            match technique {
+                Technique::WithClause(clause_index) => {
+                    MatchClause::new(solver, &env_goal, clause_index).solve()
+                }
+                Technique::WithImpl(impl_id) => {
+                    NormalizeWithImpl::new(solver, env_goal.clone(), impl_id).solve()
+                }
+            }
         });
-
-        // If we found something in the environment, even if it might
-        // have caused us to infer things that are not necessarily
-        // correct, then take it. This is not obviously the right
-        // thing to do but it models rustc's current behavior.
-        if let Ok(v) = env_result {
-            return Ok(v);
-        }
-
-        // Nothing in the environment, so try impls.
-        let impl_result = {
-            solver.solve_any(&program.impl_data, &env_goal, |solver, (&impl_id, _impl_data)| {
-                NormalizeWithImpl::new(solver, env_goal.clone(), impl_id).solve()
-            })
-        };
-        if let Ok(v) = impl_result {
+        if let Ok(v) = result {
             return Ok(v);
         }
 
