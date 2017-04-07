3871c560e57621dcbf90df5efe4c54e4966c6caf
Without Boats <woboats@gmail.com>
2017-04-07 02:39:29 -0700

    Change Instantiator to separate lists.

--- src/solve/infer/instantiate.rs
+++ src/solve/infer/instantiate.rs
@@ -1,4 +1,5 @@
 use fold::*;
+use ir::QueryBinders;
 use std::fmt::Debug;
 
 use super::*;
@@ -11,9 +12,14 @@ impl InferenceTable {
               U: IntoIterator<Item = ParameterKind<UniverseIndex>>
     {
         debug!("instantiate(arg={:?})", arg);
-        let vars: Vec<_> = universes.into_iter()
-            .map(|u| self.new_parameter_variable(u))
-            .collect();
+        let mut vars = QueryBinders::default();
+        for var in universes.into_iter().map(|u| self.new_parameter_variable(u)) {
+            match var {
+                ParameterKind::Ty(ty)       => vars.tys.push(ty),
+                ParameterKind::Lifetime(l)  => vars.lifetimes.push(l),
+                ParameterKind::Krate(krate) => vars.krates.push(krate),
+            }
+        }
         debug!("instantiate: vars={:?}", vars);
         let mut instantiator = Instantiator { vars: vars };
         arg.fold_with(&mut instantiator, 0).expect("")
@@ -21,7 +27,7 @@ impl InferenceTable {
 }
 
 struct Instantiator {
-    vars: Vec<ParameterInferenceVariable>,
+    vars: QueryBinders<TyInferenceVariable, LifetimeInferenceVariable, KrateInferenceVariable>,
 }
 
 /// Folder: when we encounter a free variable (of any kind) with index
@@ -31,26 +37,26 @@ struct Instantiator {
 /// instantiating.
 impl Folder for Instantiator {
     fn fold_free_var(&mut self, depth: usize, binders: usize) -> Result<Ty> {
-        if depth < self.vars.len() {
-            Ok(self.vars[depth].as_ref().ty().unwrap().to_ty().up_shift(binders))
+        if depth < self.vars.tys.len() {
+            Ok(self.vars.tys[depth].to_ty().up_shift(binders))
         } else {
-            Ok(Ty::Var(depth + binders - self.vars.len())) // see comment above
+            Ok(Ty::Var(depth + binders - self.vars.tys.len())) // see comment above
         }
     }
 
     fn fold_free_lifetime_var(&mut self, depth: usize, binders: usize) -> Result<Lifetime> {
-        if depth < self.vars.len() {
-            Ok(self.vars[depth].as_ref().lifetime().unwrap().to_lifetime().up_shift(binders))
+        if depth < self.vars.lifetimes.len() {
+            Ok(self.vars.lifetimes[depth].to_lifetime().up_shift(binders))
         } else {
-            Ok(Lifetime::Var(depth + binders - self.vars.len())) // see comment above
+            Ok(Lifetime::Var(depth + binders - self.vars.lifetimes.len())) // see comment above
         }
     }
 
     fn fold_free_krate_var(&mut self, depth: usize, binders: usize) -> Result<Krate> {
-        if depth < self.vars.len() {
-            Ok(self.vars[depth].as_ref().krate().unwrap().to_krate().up_shift(binders))
+        if depth < self.vars.krates.len() {
+            Ok(self.vars.krates[depth].to_krate().up_shift(binders))
         } else {
-            Ok(Krate::Var(depth + binders - self.vars.len())) // see comment above
+            Ok(Krate::Var(depth + binders - self.vars.krates.len())) // see comment above
         }
     }
 }
