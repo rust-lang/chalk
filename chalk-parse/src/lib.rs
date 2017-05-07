8ebb46dce09c3fe7b796b84d6ab5e33432d1b1cd
Niko Matsakis <niko@alum.mit.edu>
2017-05-07 06:37:07 -0400

    improve parse error output and also include a basic libstd.chalk

--- chalk-parse/src/lib.rs
+++ chalk-parse/src/lib.rs
@@ -11,6 +11,8 @@ pub mod errors;
 mod parser;
 
 use errors::Result;
+use lalrpop_util::ParseError;
+use std::fmt::Write;
 
 pub fn parse_program(text: &str) -> Result<ast::Program> {
     match parser::parse_Program(text) {
@@ -29,7 +31,28 @@ pub fn parse_ty(text: &str) -> Result<ast::Ty> {
 pub fn parse_goal(text: &str) -> Result<Box<ast::Goal>> {
     match parser::parse_Goal(text) {
         Ok(v) => Ok(v),
-        Err(e) => bail!("parse error: {:?}", e),
+        Err(e) => {
+            let position_string = |start: usize, end: usize| {
+                let mut output = String::new();
+                let text = text.replace("\n", " ").replace("\r", " ");
+                write!(output, "position: `{}`\n", text).expect("str-write cannot fail");
+                write!(output, "           ").expect("str-write cannot fail");
+                for _ in 0..start { output.push_str(" "); }
+                for _ in start..end { output.push_str("^"); }
+                output.push_str("\n");
+                output
+            };
+            match e {
+                ParseError::InvalidToken { location } =>
+                    bail!("parse error: {:?}\n{}", e, position_string(location, location+1)),
+                ParseError::UnrecognizedToken { token: Some((start, _, end)), .. } =>
+                    bail!("parse error: {:?}\n{}", e, position_string(start, end)),
+                ParseError::ExtraToken { token: (start, _, end), .. } =>
+                    bail!("parse error: {:?}\n{}", e, position_string(start, end)),
+                _ =>
+                    bail!("parse error: {:?}", e),
+            }
+        }
     }
 }
 
