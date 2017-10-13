bbf60e734977af2131e3f3e8a018118e8a51ab89
Niko Matsakis <niko@alum.mit.edu>
2017-10-13 06:25:19 -0400

    make result of folding have to be foldable

--- src/fold/mod.rs
+++ src/fold/mod.rs
@@ -166,7 +166,7 @@ pub trait Fold: Debug {
     /// values, in which case owned values are produced (for example,
     /// one can fold over a `&T` value where `T: Fold`, in which case
     /// you get back a `T`, not a `&T`).
-    type Result;
+    type Result: Fold;
 
     /// Apply the given folder `folder` to `self`; `binders` is the
     /// number of binders that are in scope when beginning the
