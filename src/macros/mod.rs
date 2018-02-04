a4e004205a94e05b473c28d93cd4a0999227f470
Niko Matsakis <niko@alum.mit.edu>
2018-02-04 06:05:17 -0500

    improve readability of multiline debug output

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -79,13 +79,15 @@ pub fn dump(string: &str, suffix: &str) {
     for line in string.lines() {
         if first {
             for _ in 0..indent {
-                eprint!("| ");
+                eprint!(": ");
             }
+            eprint!("| ");
         } else {
             eprintln!();
             for _ in 0..indent {
-                eprint!("  ");
+                eprint!(": ");
             }
+            eprint!(": ");
         }
         eprint!("{}", line);
         first = false;
