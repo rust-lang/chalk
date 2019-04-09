6afc03953e39cf3718f0996796dc1e0498f30643
Niko Matsakis <niko@alum.mit.edu>
2019-04-09 05:31:15 -0400

    add comments to `RustIrSource` trait

--- src/rules.rs
+++ src/rules.rs
@@ -9,12 +9,32 @@ use std::sync::Arc;
 pub(crate) mod wf;
 
 pub trait RustIrSource {
+    /// Returns the datum for the associated type with the given id.
     fn associated_ty_data(&self, ty: TypeId) -> Arc<AssociatedTyDatum>;
 
+    /// Returns the datum for the impl with the given id.
     fn impl_datum(&self, impl_id: ImplId) -> &ImplDatum;
 
+    /// Returns true if there is an explicit impl of the auto trait
+    /// `auto_trait_id` for the struct `struct_id`. This is part of
+    /// the auto trait handling -- if there is no explicit impl given
+    /// by the user for the struct, then we provide default impls
+    /// based on the field types (otherwise, we rely on the impls the
+    /// user gave).
     fn impl_provided_for(&self, auto_trait_id: TraitId, struct_id: StructId) -> bool;
 
+    /// Given a projection of an associated type, splits the type
+    /// parameters into two parts: those that come from the trait, and
+    /// those that come from the associated type itself.
+    ///
+    /// e.g. given a projection `<Foo as Iterable>::Item<'x>`, where `Iterable` is defined like so:
+    ///
+    /// ```ignore
+    /// trait Iterable { type Item<'a>; }
+    /// ```
+    ///
+    /// we would split into the type parameter lists `[Foo]` (from the
+    /// trait) and `['x]` (from the type).
     fn split_projection<'p>(
         &self,
         projection: &'p ProjectionTy,
