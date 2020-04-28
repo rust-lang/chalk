nikomatsakis/chalk-ndm

convert from lifetime equality constraints to outlives constraints

chalk-ir/src/lib.rs
@@ -1117,7 +1117,7 @@ pub enum QuantifierKind {
 /// checking in the compiler.
 #[derive(Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Fold, HasTypeFamily)]
 pub enum Constraint<TF: TypeFamily> {
-    LifetimeEq(Lifetime<TF>, Lifetime<TF>),
+    Outlives(Lifetime<TF>, Lifetime<TF>),
 }
 
 /// A mapping of inference variables to instantiations thereof.

rust-lang/chalk

chalk-ir/src/lib.rs
@@ -1900,7 +1900,7 @@ pub enum QuantifierKind {
 /// checking in the compiler.
 #[derive(Clone, PartialEq, Eq, Hash, Fold, Visit, HasInterner)]
 pub enum Constraint<I: Interner> {
-    LifetimeEq(Lifetime<I>, Lifetime<I>),
+    Outlives(Lifetime<I>, Lifetime<I>),
 }
 
 /// A mapping of inference variables to instantiations thereof.
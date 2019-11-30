a120069c94fc1412b0ee4b3726487c9258057a43
Niko Matsakis <niko@alum.mit.edu>
2019-11-30 00:26:05 -0500

    convert from lifetime equality constraints to outlives constraints

--- tests/test/projection.rs
+++ tests/test/projection.rs
@@ -724,7 +724,9 @@ fn normalize_under_binder() {
         } yields {
             "Unique; for<?U0> { \
              substitution [?0 := Ref<'^0.0, I32>], \
-             lifetime constraints [InEnvironment { environment: Env([]), goal: '^0.0 == '!1_0 }] \
+             lifetime constraints [\
+             InEnvironment { environment: Env([]), goal: '!1_0: '^0.0 }, \
+             InEnvironment { environment: Env([]), goal: '^0.0: '!1_0 }] \
              }"
         }
     }
@@ -754,7 +756,11 @@ fn normalize_under_binder_multi() {
             }
         } yields_all {
             "substitution [?0 := I32], lifetime constraints []",
-            "for<?U0,?U0> { substitution [?0 := (Deref::Item)<Ref<'^0.0, I32>, '^0.1>], lifetime constraints [InEnvironment { environment: Env([]), goal: '^0.0 == '!1_0 }, InEnvironment { environment: Env([]), goal: '^0.1 == '!1_0 }] }"
+            "for<?U0,?U0> { substitution [?0 := (Deref::Item)<Ref<'^0.0, I32>, '^0.1>], lifetime constraints [\
+            InEnvironment { environment: Env([]), goal: '^0.0: '!1_0 }, \
+            InEnvironment { environment: Env([]), goal: '!1_0: '^0.0 }, \
+            InEnvironment { environment: Env([]), goal: '^0.1: '!1_0 }, \
+            InEnvironment { environment: Env([]), goal: '!1_0: '^0.1 }] }"
         }
 
         goal {
