4d440e9b8fdcaeba742a0e580e482a04b3a79ad5
nathanwhit <nathan.whitaker01@gmail.com>
2020-05-08 00:46:56 -0400

    Add tests ensuring slices are not `Copy`/`Clone`

--- tests/test/slices.rs
+++ tests/test/slices.rs
@@ -29,3 +29,35 @@ fn slices_are_well_formed() {
         }
     }
 }
+
+#[test]
+fn slices_are_not_copy() {
+    test! {
+        program {
+            #[lang(copy)]
+            trait Copy { }
+        }
+
+        goal {
+            forall<T> { not { [T]: Copy } }
+        } yields {
+            "Unique; substitution [], lifetime constraints []"
+        }
+    }
+}
+
+#[test]
+fn slices_are_not_clone() {
+    test! {
+        program {
+            #[lang(clone)]
+            trait Clone { }
+        }
+
+        goal {
+            forall<T> { not { [T]: Clone } }
+        } yields {
+            "Unique; substitution [], lifetime constraints []"
+        }
+    }
+}
