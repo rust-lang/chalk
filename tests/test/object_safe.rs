7360bbd6516dd963573eb17c55e0800592d5d837
Mcat12 <newtoncat12@yahoo.com>
2020-05-09 18:12:11 -0400

    Simplify ObjectSafe tests

--- tests/test/object_safe.rs
+++ tests/test/object_safe.rs
@@ -1,29 +1,15 @@
 use super::*;
 
-/// The current implementation of RustIrDatabase::is_object_safe only checks for
-/// the #[object_safe] flag. This test will start failing once it performs a
-/// proper test because Foo should be object safe.
-#[test]
-fn by_default_not_object_safe() {
-    test! {
-        program {
-            trait Foo {}
-        }
-
-        goal { ObjectSafe(Foo) } yields { "No possible solution" }
-        goal { not { ObjectSafe(Foo) } } yields { "Unique" }
-    }
-}
-
 #[test]
 fn object_safe_flag() {
     test! {
         program {
             #[object_safe]
             trait Foo {}
+            trait Bar {}
         }
 
         goal { ObjectSafe(Foo) } yields { "Unique" }
-        goal { not { ObjectSafe(Foo) } } yields { "No possible solution" }
+        goal { not { ObjectSafe(Bar) } } yields { "Unique" }
     }
 }
