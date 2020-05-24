62b40fd3d59c91e39d6cba5c1af9297edbc8c7f3
Super Tuple <supertuple@gmail.com>
2020-05-23 17:27:06 -0700

    Add smart pointer support to LoggingDatabase
    Allows wrapping a RustIrDatabase that is behind a smart pointer such as
    Arc.
    
    Co-authored-by: David Ross <daboross@daboross.net>

--- chalk-solve/src/logging_db.rs
+++ chalk-solve/src/logging_db.rs
@@ -1,55 +1,66 @@
 //! Provides wrappers over `RustIrDatabase` which record used definitions and write
 //! `.chalk` files containing those definitions.
 use std::{
-    cmp::Ord, cmp::Ordering, collections::BTreeSet, fmt, fmt::Display, io::Write, sync::Arc,
-    sync::Mutex,
+    borrow::Borrow, cmp::Ord, cmp::Ordering, collections::BTreeSet, fmt, fmt::Display, io::Write,
+    marker::PhantomData, sync::Arc, sync::Mutex,
 };
 
 use crate::rust_ir::*;
 use chalk_ir::{interner::Interner, *};
 
 use crate::{display, RustIrDatabase};
-/// Wraps another `RustIrDatabase` (`DB`) and records which definitions are used.
+use fmt::Debug;
+/// Wraps another `RustIrDatabase` (`DB`) and records which definitions are
+/// used.
 ///
 /// A full .chalk file containing all used definitions can be recovered through
 /// `LoggingRustIrDatabase`'s `Display` implementation.
+///
+/// Uses a separate type, `P`, for the database stored inside to account for
+/// `Arc` or wrapping other storage mediums.
 #[derive(Debug)]
-pub struct LoggingRustIrDatabase<I, DB>
+pub struct LoggingRustIrDatabase<I, DB, P = DB>
 where
     DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
     I: Interner,
 {
-    db: DB,
+    db: P,
     def_ids: Mutex<BTreeSet<RecordedItemId<I>>>,
+    _phantom: PhantomData<DB>,
 }
 
-impl<I, DB> LoggingRustIrDatabase<I, DB>
+impl<I, DB, P> LoggingRustIrDatabase<I, DB, P>
 where
     DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
     I: Interner,
 {
-    pub fn new(db: DB) -> Self {
+    pub fn new(db: P) -> Self {
         LoggingRustIrDatabase {
             db,
             def_ids: Default::default(),
+            _phantom: PhantomData,
         }
     }
 }
 
-impl<I, DB> Display for LoggingRustIrDatabase<I, DB>
+impl<I, DB, P> Display for LoggingRustIrDatabase<I, DB, P>
 where
     DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
     I: Interner,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let def_ids = self.def_ids.lock().unwrap();
-        display::write_program(f, &self.db, def_ids.iter().copied())
+        display::write_program(f, self.db.borrow(), def_ids.iter().copied())
     }
 }
 
-impl<I, DB> LoggingRustIrDatabase<I, DB>
+impl<I, DB, P> LoggingRustIrDatabase<I, DB, P>
 where
     DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
     I: Interner,
 {
     fn record(&self, id: impl Into<RecordedItemId<I>>) {
@@ -67,45 +78,46 @@ where
     }
 }
 
-impl<I, DB> RustIrDatabase<I> for LoggingRustIrDatabase<I, DB>
+impl<I, DB, P> RustIrDatabase<I> for LoggingRustIrDatabase<I, DB, P>
 where
     DB: RustIrDatabase<I>,
+    P: Borrow<DB> + Debug,
     I: Interner,
 {
     fn custom_clauses(&self) -> Vec<chalk_ir::ProgramClause<I>> {
-        self.db.custom_clauses()
+        self.db.borrow().custom_clauses()
     }
     fn associated_ty_data(
         &self,
         ty: chalk_ir::AssocTypeId<I>,
     ) -> Arc<crate::rust_ir::AssociatedTyDatum<I>> {
-        let ty_datum = self.db.associated_ty_data(ty);
+        let ty_datum = self.db.borrow().associated_ty_data(ty);
         self.record(ty_datum.trait_id);
         ty_datum
     }
     fn trait_datum(&self, trait_id: TraitId<I>) -> Arc<TraitDatum<I>> {
         self.record(trait_id);
-        self.db.trait_datum(trait_id)
+        self.db.borrow().trait_datum(trait_id)
     }
     fn adt_datum(&self, adt_id: AdtId<I>) -> Arc<AdtDatum<I>> {
         self.record(adt_id);
-        self.db.adt_datum(adt_id)
+        self.db.borrow().adt_datum(adt_id)
     }
     fn impl_datum(&self, impl_id: ImplId<I>) -> Arc<ImplDatum<I>> {
         self.record(impl_id);
-        self.db.impl_datum(impl_id)
+        self.db.borrow().impl_datum(impl_id)
     }
     fn associated_ty_value(
         &self,
         id: crate::rust_ir::AssociatedTyValueId<I>,
     ) -> Arc<crate::rust_ir::AssociatedTyValue<I>> {
-        let value = self.db.associated_ty_value(id);
+        let value = self.db.borrow().associated_ty_value(id);
         self.record(value.impl_id);
         value
     }
     fn opaque_ty_data(&self, id: OpaqueTyId<I>) -> Arc<OpaqueTyDatum<I>> {
         self.record(id);
-        self.db.opaque_ty_data(id)
+        self.db.borrow().opaque_ty_data(id)
     }
     fn impls_for_trait(
         &self,
@@ -113,24 +125,24 @@ where
         parameters: &[chalk_ir::GenericArg<I>],
     ) -> Vec<ImplId<I>> {
         self.record(trait_id);
-        let impl_ids = self.db.impls_for_trait(trait_id, parameters);
+        let impl_ids = self.db.borrow().impls_for_trait(trait_id, parameters);
         self.record_all(impl_ids.iter().copied());
         impl_ids
     }
     fn local_impls_to_coherence_check(&self, trait_id: TraitId<I>) -> Vec<ImplId<I>> {
         self.record(trait_id);
-        self.db.local_impls_to_coherence_check(trait_id)
+        self.db.borrow().local_impls_to_coherence_check(trait_id)
     }
     fn impl_provided_for(&self, auto_trait_id: TraitId<I>, adt_id: AdtId<I>) -> bool {
         self.record(auto_trait_id);
         self.record(adt_id);
-        self.db.impl_provided_for(auto_trait_id, adt_id)
+        self.db.borrow().impl_provided_for(auto_trait_id, adt_id)
     }
     fn well_known_trait_id(
         &self,
         well_known_trait: crate::rust_ir::WellKnownTrait,
     ) -> Option<TraitId<I>> {
-        let trait_id = self.db.well_known_trait_id(well_known_trait);
+        let trait_id = self.db.borrow().well_known_trait_id(well_known_trait);
         trait_id.map(|id| self.record(id));
         trait_id
     }
@@ -138,29 +150,29 @@ where
         &self,
         environment: &chalk_ir::Environment<I>,
     ) -> chalk_ir::ProgramClauses<I> {
-        self.db.program_clauses_for_env(environment)
+        self.db.borrow().program_clauses_for_env(environment)
     }
     fn interner(&self) -> &I {
-        self.db.interner()
+        self.db.borrow().interner()
     }
     fn trait_name(&self, trait_id: TraitId<I>) -> String {
-        self.db.trait_name(trait_id)
+        self.db.borrow().trait_name(trait_id)
     }
     fn adt_name(&self, adt_id: AdtId<I>) -> String {
-        self.db.adt_name(adt_id)
+        self.db.borrow().adt_name(adt_id)
     }
     fn assoc_type_name(&self, assoc_ty_id: AssocTypeId<I>) -> String {
-        self.db.assoc_type_name(assoc_ty_id)
+        self.db.borrow().assoc_type_name(assoc_ty_id)
     }
     fn opaque_type_name(&self, opaque_ty_id: OpaqueTyId<I>) -> String {
-        self.db.opaque_type_name(opaque_ty_id)
+        self.db.borrow().opaque_type_name(opaque_ty_id)
     }
     fn is_object_safe(&self, trait_id: TraitId<I>) -> bool {
         self.record(trait_id);
-        self.db.is_object_safe(trait_id)
+        self.db.borrow().is_object_safe(trait_id)
     }
     fn fn_def_datum(&self, fn_def_id: chalk_ir::FnDefId<I>) -> Arc<FnDefDatum<I>> {
-        todo!()
+        todo!("function def datum")
     }
 }
 
@@ -168,21 +180,26 @@ where
 /// definition to the given file.
 ///
 /// Uses [`LoggingRustIrDatabase`] internally.
-pub struct WriteOnDropRustIrDatabase<I, DB, W>
+///
+/// Uses a separate type, `P`, for the database stored inside to account for
+/// `Arc` or wrapping other storage mediums.
+pub struct WriteOnDropRustIrDatabase<I, W, DB, P = DB>
 where
-    DB: RustIrDatabase<I>,
     I: Interner,
     W: Write,
+    DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
 {
-    db: LoggingRustIrDatabase<I, DB>,
+    db: LoggingRustIrDatabase<I, DB, P>,
     write: W,
 }
 
-impl<I, DB, W> fmt::Debug for WriteOnDropRustIrDatabase<I, DB, W>
+impl<I, W, DB, P> fmt::Debug for WriteOnDropRustIrDatabase<I, W, DB, P>
 where
     I: Interner,
-    DB: RustIrDatabase<I> + fmt::Debug,
     W: Write,
+    DB: RustIrDatabase<I>,
+    P: Borrow<DB> + fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("WriteOnDropRustIrDatabase")
@@ -192,29 +209,31 @@ where
     }
 }
 
-impl<I, DB, W> WriteOnDropRustIrDatabase<I, DB, W>
+impl<I, W, DB, P> WriteOnDropRustIrDatabase<I, W, DB, P>
 where
-    DB: RustIrDatabase<I>,
     I: Interner,
     W: Write,
+    DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
 {
-    pub fn new(db: DB, write: W) -> Self {
+    pub fn new(db: P, write: W) -> Self {
         WriteOnDropRustIrDatabase {
             db: LoggingRustIrDatabase::new(db),
             write,
         }
     }
 
-    pub fn from_logging_db(db: LoggingRustIrDatabase<I, DB>, write: W) -> Self {
+    pub fn from_logging_db(db: LoggingRustIrDatabase<I, DB, P>, write: W) -> Self {
         WriteOnDropRustIrDatabase { db, write }
     }
 }
 
-impl<I, DB, W> Drop for WriteOnDropRustIrDatabase<I, DB, W>
+impl<I, W, DB, P> Drop for WriteOnDropRustIrDatabase<I, W, DB, P>
 where
-    DB: RustIrDatabase<I>,
     I: Interner,
     W: Write,
+    DB: RustIrDatabase<I>,
+    P: Borrow<DB>,
 {
     fn drop(&mut self) {
         write!(self.write, "{}", self.db)
@@ -223,84 +242,85 @@ where
     }
 }
 
-impl<I, DB, W> RustIrDatabase<I> for WriteOnDropRustIrDatabase<I, DB, W>
+impl<I, W, DB, P> RustIrDatabase<I> for WriteOnDropRustIrDatabase<I, W, DB, P>
 where
-    DB: RustIrDatabase<I>,
-    W: Write,
     I: Interner,
+    W: Write,
+    DB: RustIrDatabase<I>,
+    P: Borrow<DB> + Debug,
 {
     fn custom_clauses(&self) -> Vec<chalk_ir::ProgramClause<I>> {
-        self.db.custom_clauses()
+        self.db.borrow().custom_clauses()
     }
     fn associated_ty_data(
         &self,
         ty: chalk_ir::AssocTypeId<I>,
     ) -> Arc<crate::rust_ir::AssociatedTyDatum<I>> {
-        self.db.associated_ty_data(ty)
+        self.db.borrow().associated_ty_data(ty)
     }
     fn trait_datum(&self, trait_id: TraitId<I>) -> Arc<TraitDatum<I>> {
-        self.db.trait_datum(trait_id)
+        self.db.borrow().trait_datum(trait_id)
     }
     fn adt_datum(&self, adt_id: AdtId<I>) -> Arc<AdtDatum<I>> {
-        self.db.adt_datum(adt_id)
+        self.db.borrow().adt_datum(adt_id)
     }
     fn impl_datum(&self, impl_id: ImplId<I>) -> Arc<ImplDatum<I>> {
-        self.db.impl_datum(impl_id)
+        self.db.borrow().impl_datum(impl_id)
     }
     fn associated_ty_value(
         &self,
         id: crate::rust_ir::AssociatedTyValueId<I>,
     ) -> Arc<crate::rust_ir::AssociatedTyValue<I>> {
-        self.db.associated_ty_value(id)
+        self.db.borrow().associated_ty_value(id)
     }
     fn opaque_ty_data(&self, id: OpaqueTyId<I>) -> Arc<OpaqueTyDatum<I>> {
-        self.db.opaque_ty_data(id)
+        self.db.borrow().opaque_ty_data(id)
     }
     fn impls_for_trait(
         &self,
         trait_id: TraitId<I>,
         parameters: &[chalk_ir::GenericArg<I>],
     ) -> Vec<ImplId<I>> {
-        self.db.impls_for_trait(trait_id, parameters)
+        self.db.borrow().impls_for_trait(trait_id, parameters)
     }
     fn local_impls_to_coherence_check(&self, trait_id: TraitId<I>) -> Vec<ImplId<I>> {
-        self.db.local_impls_to_coherence_check(trait_id)
+        self.db.borrow().local_impls_to_coherence_check(trait_id)
     }
     fn impl_provided_for(&self, auto_trait_id: TraitId<I>, adt_id: AdtId<I>) -> bool {
-        self.db.impl_provided_for(auto_trait_id, adt_id)
+        self.db.borrow().impl_provided_for(auto_trait_id, adt_id)
     }
     fn well_known_trait_id(
         &self,
         well_known_trait: crate::rust_ir::WellKnownTrait,
     ) -> Option<TraitId<I>> {
-        self.db.well_known_trait_id(well_known_trait)
+        self.db.borrow().well_known_trait_id(well_known_trait)
     }
     fn program_clauses_for_env(
         &self,
         environment: &chalk_ir::Environment<I>,
     ) -> chalk_ir::ProgramClauses<I> {
-        self.db.program_clauses_for_env(environment)
+        self.db.borrow().program_clauses_for_env(environment)
     }
     fn interner(&self) -> &I {
-        self.db.interner()
+        self.db.borrow().interner()
     }
     fn is_object_safe(&self, trait_id: TraitId<I>) -> bool {
-        self.db.is_object_safe(trait_id)
+        self.db.borrow().is_object_safe(trait_id)
     }
     fn trait_name(&self, trait_id: TraitId<I>) -> String {
-        self.db.trait_name(trait_id)
+        self.db.borrow().trait_name(trait_id)
     }
     fn adt_name(&self, adt_id: AdtId<I>) -> String {
-        self.db.adt_name(adt_id)
+        self.db.borrow().adt_name(adt_id)
     }
     fn assoc_type_name(&self, assoc_ty_id: AssocTypeId<I>) -> String {
-        self.db.assoc_type_name(assoc_ty_id)
+        self.db.borrow().assoc_type_name(assoc_ty_id)
     }
     fn opaque_type_name(&self, opaque_ty_id: OpaqueTyId<I>) -> String {
-        self.db.opaque_type_name(opaque_ty_id)
+        self.db.borrow().opaque_type_name(opaque_ty_id)
     }
     fn fn_def_datum(&self, fn_def_id: chalk_ir::FnDefId<I>) -> Arc<FnDefDatum<I>> {
-        todo!()
+        todo!("function def datum")
     }
 }
 
