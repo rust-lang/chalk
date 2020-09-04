8ecd0c9722d41bf6a80b618b4e868f3fdf62374f
memoryleak47 <memoryleak47@protonmail.ch>
2020-09-04 21:09:59 +0200

    Improve extern type tests

--- tests/test/extern_types.rs
+++ tests/test/extern_types.rs
@@ -2,11 +2,12 @@
 
 use super::*;
 
+// extern types don't implement any builtin traits
 #[test]
 fn extern_ty_trait_impl() {
     test! {
         program {
-            extern { type A; }
+            extern type A;
             trait Foo {}
             impl Foo for A {}
         }
@@ -16,55 +17,59 @@ fn extern_ty_trait_impl() {
 }
 
 #[test]
-fn extern_ty_single() {
+fn extern_ty_lowering() {
     lowering_success! {
         program {
-            extern { type A; }
+            extern type A;
         }
     }
 }
 
+// extern types are always well-formed
 #[test]
 fn extern_ty_is_well_formed() {
     test! {
         program {
-            extern { type A; }
+            extern type A;
         }
 
         goal { WellFormed(A) } yields { "Unique" }
     }
 }
 
+// extern types don't implement any builtin traits
 #[test]
 fn extern_ty_is_not_sized() {
     test! {
         program {
             #[lang(sized)] trait Sized {}
-            extern { type A; }
+            extern type A;
         }
 
         goal { not { A: Sized } } yields { "Unique" }
     }
 }
 
+// extern types don't implement any builtin traits
 #[test]
 fn extern_ty_is_not_copy() {
     test! {
         program {
             #[lang(copy)] trait Copy {}
-            extern { type A; }
+            extern type A;
         }
 
         goal { not { A: Copy } } yields { "Unique" }
     }
 }
 
+// extern types don't implement any builtin traits
 #[test]
 fn extern_ty_is_not_clone() {
     test! {
         program {
             #[lang(clone)] trait Clone {}
-            extern { type A; }
+            extern type A;
         }
 
         goal { not { A: Clone } } yields { "Unique" }
