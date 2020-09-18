6bd2da5b435a01e3d2d489ccbf1747f6269594aa
memoryleak47 <memoryleak47@protonmail.ch>
2020-09-18 22:07:58 +0200

    Add `consistuent_types`

--- chalk-solve/src/clauses.rs
+++ chalk-solve/src/clauses.rs
@@ -18,6 +18,45 @@ mod env_elaborator;
 mod generalize;
 pub mod program_clauses;
 
+// yields the types "contained" in `app_ty`
+fn consistuent_types<I: Interner>(
+    db: &dyn RustIrDatabase<I>,
+    app_ty: &ApplicationTy<I>,
+) -> Vec<Ty<I>> {
+    let interner = db.interner();
+    match app_ty.name {
+        TypeName::Adt(adt_id) => {
+            let adt_datum = &db.adt_datum(adt_id);
+            let adt_datum_bound = adt_datum.binders.substitute(interner, &app_ty.substitution);
+            adt_datum_bound
+                .variants
+                .into_iter()
+                .map(|variant| variant.fields.into_iter())
+                .flatten()
+                .collect()
+        }
+        TypeName::Array
+        | TypeName::Tuple(_)
+        | TypeName::Slice
+        | TypeName::Raw(_)
+        | TypeName::Ref(_)
+        | TypeName::Scalar(_)
+        | TypeName::Str
+        | TypeName::Never
+        | TypeName::FnDef(_) => app_ty
+            .substitution
+            .iter(interner)
+            .filter_map(|x| x.ty(interner))
+            .cloned()
+            .collect(),
+        TypeName::AssociatedType(_) => unimplemented!(),
+        TypeName::Closure(_) => unimplemented!(),
+        TypeName::OpaqueType(_) => unimplemented!(),
+        TypeName::Foreign(_) => unimplemented!(),
+        TypeName::Error => Vec::new(),
+    }
+}
+
 /// FIXME(#505) update comments for ADTs
 /// For auto-traits, we generate a default rule for every struct,
 /// unless there is a manual impl for that struct given explicitly.
@@ -77,139 +116,17 @@ pub fn push_auto_trait_impls<I: Interner>(
     // we assume that the builder has no binders so far.
     assert!(builder.placeholders_in_scope().is_empty());
 
-    match app_ty.name {
-        TypeName::Adt(adt_id) => {
-            let adt_datum = &builder.db.adt_datum(adt_id);
+    let mk_ref = |ty: Ty<I>| TraitRef {
+        trait_id: auto_trait_id,
+        substitution: Substitution::from1(interner, ty.cast(interner)),
+    };
+    let consequence = mk_ref(app_ty.clone().intern(interner));
 
-            let binders = adt_datum.binders.map_ref(|b| &b.variants);
-            builder.push_binders(&binders, |builder, variants| {
-                let self_ty: Ty<_> = ApplicationTy {
-                    name: adt_id.cast(interner),
-                    substitution: builder.substitution_in_scope(),
-                }
-                .intern(interner);
+    let conditions = consistuent_types(builder.db, app_ty)
+        .into_iter()
+        .map(mk_ref);
 
-                // trait_ref = `MyStruct<...>: MyAutoTrait`
-                let auto_trait_ref = TraitRef {
-                    trait_id: auto_trait_id,
-                    substitution: Substitution::from1(interner, self_ty),
-                };
-
-                // forall<P0..Pn> { // generic parameters from struct
-                //   MyStruct<...>: MyAutoTrait :-
-                //      Field0: MyAutoTrait,
-                //      ...
-                //      FieldN: MyAutoTrait
-                // }
-                builder.push_clause(
-                    auto_trait_ref,
-                    variants.iter().flat_map(|variant| {
-                        variant.fields.iter().map(|field_ty| TraitRef {
-                            trait_id: auto_trait_id,
-                            substitution: Substitution::from1(interner, field_ty.clone()),
-                        })
-                    }),
-                );
-            });
-        }
-        TypeName::Array => {
-            let len_type: Ty<_> = ApplicationTy {
-                name: TypeName::Scalar(Scalar::Uint(UintTy::U32)),
-                substitution: Substitution::empty(interner),
-            }
-            .intern(interner);
-            let varkinds_iter = iter::once(VariableKind::Ty(TyKind::General))
-                .chain(iter::once(VariableKind::Const(len_type)));
-            let binders = Binders::new(
-                VariableKinds::from_iter(interner, varkinds_iter),
-                std::marker::PhantomData::<I>,
-            );
-
-            builder.push_binders(&binders, |builder, _| {
-                let sub = builder.substitution_in_scope();
-                let arg: GenericArg<_> = sub.at(interner, 0).clone();
-
-                // self_ty = [P; n]
-                let self_ty: GenericArg<_> = ApplicationTy {
-                    name: TypeName::Array,
-                    substitution: sub,
-                }
-                .intern(interner)
-                .cast(interner);
-
-                // consequence = `[P; n]: MyAutoTrait`
-                let consequence = TraitRef {
-                    trait_id: auto_trait_id,
-                    substitution: Substitution::from1(interner, self_ty),
-                };
-
-                // condition = `P: MyAutoTrait`
-                let condition = TraitRef {
-                    trait_id: auto_trait_id,
-                    substitution: Substitution::from1(interner, arg),
-                };
-
-                // forall<P, n> {
-                //    [P; n]: MyAutoTrait :- P: MyAutoTrait
-                // }
-                builder.push_clause(consequence, iter::once(condition));
-            });
-        }
-        // non-atomic TypeName-variants which implement AutoTrait if and only if all types from `app_ty.substitution` implement AutoTrait
-        tn @ TypeName::Tuple(_) | tn @ TypeName::Slice | tn @ TypeName::Raw(_) => {
-            let n = app_ty.substitution.len(interner);
-            let binders = Binders::new(
-                VariableKinds::from_iter(
-                    interner,
-                    iter::repeat(VariableKind::Ty(TyKind::General)).take(n),
-                ),
-                std::marker::PhantomData::<I>,
-            );
-            builder.push_binders(&binders, |builder, _| {
-                let sub = builder.substitution_in_scope();
-
-                let self_ty: GenericArg<_> = ApplicationTy {
-                    name: tn,
-                    substitution: sub.clone(),
-                }
-                .intern(interner)
-                .cast(interner);
-
-                let consequence = TraitRef {
-                    trait_id: auto_trait_id,
-                    substitution: Substitution::from1(interner, self_ty),
-                };
-
-                let conditions = sub.iter(interner).map(|x| TraitRef {
-                    trait_id: auto_trait_id,
-                    substitution: Substitution::from1(interner, x.clone()),
-                });
-
-                // forall<P0, ..., Pn> { (P0, ..., Pn): MyAutoTrait :- P0: MyAutoTrait, ..., Pn: MyAutoTrait }
-                // forall<P> { [P]: MyAutoTrait :- P: MyAutoTrait }
-                // forall<P> { *const P: MyAutoTrait :- P: MyAutoTrait }
-                builder.push_clause(consequence, conditions);
-            });
-        }
-        // atomic TypeName-variants
-        TypeName::Scalar(_) | TypeName::Str | TypeName::Never | TypeName::FnDef(_) => {
-            let self_ty: GenericArg<_> = app_ty.clone().intern(interner).cast(interner);
-            let auto_trait_ref = TraitRef {
-                trait_id: auto_trait_id,
-                substitution: Substitution::from1(interner, self_ty),
-            };
-
-            builder.push_fact(auto_trait_ref);
-        }
-        TypeName::Ref(_) => {
-            unimplemented!();
-        }
-        TypeName::AssociatedType(_) => unimplemented!(),
-        TypeName::Closure(_) => unimplemented!(),
-        TypeName::OpaqueType(_) => unimplemented!(),
-        TypeName::Foreign(_) => unimplemented!(),
-        TypeName::Error => {}
-    }
+    builder.push_clause(consequence, conditions);
 }
 
 /// Leak auto traits for opaque types, just like `push_auto_trait_impls` does for structs.
